3177c54db0954fc0541309cae005903bd38ba727
maialino123 <Nguyenbatu10111@gmai.com>
2021-08-20 08:27:05 +0700

    This is my first commit

---
+++ Eproject-Online-floral-delivery/Eproject-Online-floral-delivery/Controllers/PaymentController.cs
@@ -0,0 +1,32 @@
+﻿using Eproject_Online_floral_delivery.common.payment;
+using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eproject_Online_floral_delivery.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult PaymentWithPayPal()
+        {
+            //Initialize APIContext
+            APIContext apiContext = PaypalConfiguration.GetAPIContext();
+            try
+            {
+                string PayerId = Request.Params["PayerID"];
+                if(string.IsNullOrEmpty(PayerId))
+                {
+                    string baseURI = Request.Url.Scheme + "://" + Request.Url.Authority + "PaymentWithPayPal/Paypa";
+                }
+            } catch(Exception )
+            {
+
+            }
+            return View();
+        }
+    }
+}
